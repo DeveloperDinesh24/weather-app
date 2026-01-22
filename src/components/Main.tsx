0cf2ec0699049598005831fb0f94aa5e181b00be
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-22 14:35:32 +0530

    feat: add search bar

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,10 @@
+import { SearchBar } from "./SearchBar"
+
 export const Main = () => {
   return (
     <main className="w-full mt-12">
-      <h1 className="text-center text-4xl font-semibold">How's the sky looking today?</h1>
+      <h1 className="text-center text-4xl font-semibold mb-16">How's the sky looking today?</h1>
+      <SearchBar />
     </main>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,14 @@
 export const SearchBar = () => {
   return (
-    <div>
-      <input type="text" />
-      <button>Search</button>
+    <div className="w-full flex gap-4 items-center justify-center h-10">
+      <div className="w-120 h-10 bg-[#25253f] rounded-md flex px-4">
+        <img src="../assets/images/icon-search.svg" className="w-5" />
+        <input 
+        className="w-full h-full bg-transparent outline-none px-2 text-white"
+        type="text" 
+        placeholder="Search for a place..." />
+      </div>
+      <button className="w-fit h-full text-xl px-4 bg-[#4455da] rounded-md cursor-pointer">Search</button>
     </div>
   )
 }
