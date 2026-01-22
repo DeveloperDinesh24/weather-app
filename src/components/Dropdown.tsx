3ae0968a98657d12704685697bc01af209a6bfbf
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-22 14:16:35 +0530

    feat: complete dropdown menu

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,17 +1,50 @@
+import { useState } from 'react'
+
 export const Dropdown = () => {
+  const [isDropdownActive, setIsDropdownActive] = useState<boolean>(false)
+
   return (
     <>
-      <div className='flex gap-4 bg-[#25253f] rounded-xl px-4 py-2 select-none cursor-pointer'>
+      <div
+        onClick={() => setIsDropdownActive((prev: boolean): boolean => !prev)}
+        className='flex gap-4 bg-[#25253f] rounded-md px-4 py-2 select-none cursor-pointer items-center'
+      >
         <img src='assets/images/icon-units.svg' />
         <p className='text-xl'>Units</p>
         <img src='assets/images/icon-dropdown.svg' />
       </div>
-      <div className='w-fit px-2 py-4 bg-[#25253f] rounded-xl select-none cursor-pointer'>
-        <p>Switch to Imperial</p>
-        <div>
-          <p>Temperature</p>
-          <p>Celsius(°C)</p>
-          <p>Fahrenheit(°F)</p>
+      <div
+        className={`w-42 p-2 cursor-pointer bg-[#25253f] rounded-xl select-none absolute ${isDropdownActive ? 'flex' : 'hidden'} top-20 right-0 mt-4 flex-col shadow-lg gap-2 z-10 transition-all duration-500`}
+      >
+        <p className='p-2 cursor-pointer'>Switch to Imperial</p>
+        <div className='flex flex-col gap-0.5'>
+          <p className='text-sm text-[#aeaeb7]'>Temperature</p>
+          <p className='p-2 rounded-md hover:bg-[#ffffff10] transition-colors duration-150 cursor-pointer'>
+            Celsius (°C)
+          </p>
+          <p className='p-2 rounded-md hover:bg-[#ffffff10] transition-colors duration-150 cursor-pointer'>
+            Fahrenheit (°F)
+          </p>
+        </div>
+        <hr className='text-[#ffffff10]' />
+        <div className='flex flex-col gap-0.5'>
+          <p className='text-sm text-[#aeaeb7]'>Wind Speed</p>
+          <p className='p-2 rounded-md hover:bg-[#ffffff10] transition-colors duration-150 cursor-pointer'>
+            km/h
+          </p>
+          <p className='p-2 rounded-md hover:bg-[#ffffff10] transition-colors duration-150 cursor-pointer'>
+            mph
+          </p>
+        </div>
+        <hr className='text-[#ffffff10]' />
+        <div className='flex flex-col gap-0.5'>
+          <p className='text-sm text-[#aeaeb7]'>Precipitation</p>
+          <p className='p-2 rounded-md hover:bg-[#ffffff10] transition-colors duration-150 cursor-pointer'>
+            Milimeters (mm)
+          </p>
+          <p className='p-2 rounded-md hover:bg-[#ffffff10] transition-colors duration-150 cursor-pointer'>
+            Inches (in)
+          </p>
         </div>
       </div>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Dropdown } from "./Dropdown"
 
 export const Header = () => {
   return (
-    <header className="w-full h-fit py-10 flex justify-between items-center text-white">
+    <header className="w-full h-fit py-10 flex justify-between items-center text-white relative">
       <div>
         <img src='assets/images/logo.svg' alt='logo text and image' />
       </div>
